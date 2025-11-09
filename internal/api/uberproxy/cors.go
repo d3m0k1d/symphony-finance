506f2d8d99f6a3d33b1ab55a8ae48e641214aa45
zero@thinky <shinyzero0@gmail.com>
2025-11-09 18:57:23 +0300

    fix(api/cors): add x-bank-id header to allowed

--- internal/api/uberproxy/cors.go
+++ internal/api/uberproxy/cors.go
@@ -6,7 +6,7 @@ func (s server) corsMiddleware(next http.Handler) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Access-Control-Allow-Origin", s.corsOrigin)
 		w.Header().Set("Access-Control-Allow-Credentials", "true")
-		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, Authorization, X-Bank-Id")
 		w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, GET, POST, PATCH, DELETE, PUT")
 
 		if r.Method == "OPTIONS" {
